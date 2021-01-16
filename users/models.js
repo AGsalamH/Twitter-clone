e3c7f6b691661d24635c76c1aaa39a658b4ac919
Ahmed Gamal <agsalamh@gmail.com>
2021-01-16 23:29:13 +0200

    Create user model

---
+++ users/models.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    profilePictue: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    followings: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    followers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
