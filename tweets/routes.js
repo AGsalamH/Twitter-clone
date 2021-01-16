c61da7bcd07e7610ccd532718a4d8e8f81ad2452
Ahmed Gamal <agsalamh@gmail.com>
2021-01-17 00:54:53 +0200

    Routes starter

---
+++ tweets/routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+/*
+    Tweets Routes:
+        - 
+*/
+
+module.exports = router;
--- users/routes.js
+++ users/routes.js
@@ -1,14 +1,7 @@
 const router = require('express').Router();
-const {login, signup} = require('./auth/authentication'); 
-const {loginValidationRules, signupValidationRules, validate} = require('../utils/validation')
 /*
-    Auth Routes:
-        - /user/login
-        - /user/signup
+    User Routes:
+        - 
 */
-
-router.post('/login', loginValidationRules(), validate, login);
-router.post('/signup', signupValidationRules(), validate, signup);
-
 
 module.exports = router;
