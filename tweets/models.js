1e1ed2320d28edf10ad1a17769ffa8b3974d088e
Ahmed Gamal <agsalamh@gmail.com>
2021-01-17 00:16:03 +0200

    Create Tweet model

---
+++ tweets/models.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tweetSchema = new Schema({
+    content:{
+        type: String,
+        required: true
+    },
+
+    creator:{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+
+    // likes: {
+    //     type: Number,
+    //     default: 0,
+    // },
+
+    // retweets: {
+    //     type: Number,
+    //     default: 0,
+    // },
+
+    replies:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Tweet'
+        }
+    ],
+
+    quotes:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Tweet'
+        }
+    ],
+
+    likes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+
+    retweetes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+
+    imageUrl: {
+        type: String,
+        default: null
+    },
+
+    isReply:{
+        type: Boolean,
+        default: false
+    },
+
+    isQuote:{
+        type: Boolean,
+        default: false
+    }
+},
+{ timestamps: true })
+
+// console.log(tweetSchema)
+// module.exports.tweetSchema = tweetSchema;
+module.exports = mongoose.model('Tweet',tweetSchema);
