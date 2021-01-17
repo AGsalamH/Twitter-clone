6cdfdb101091a92a23a2050bd3a3c9c36216f2e8
Ahmed Gamal <agsalamh@gmail.com>
2021-01-17 02:23:07 +0200

    Create all Endpoints

--- tweets/routes.js
+++ tweets/routes.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
+const controller = require('./controller');
+const actions = require('./actions');
+
+const {validate, tweetValidationRules} = require('../utils/validation');
+
 /*
-    Tweets Routes:
-        - 
+    Tweet Routes:
+        - GET /tweet
+        - POST /tweet
+        - DELETE /tweet
 */
+router.get('/tweet', controller.getTweet);
+router.post('/tweet',tweetValidationRules, validate, controller.createTweet);
+router.delete('/tweet', controller.deleteTweet);
+
+/*
+    Actions Routes:
+        - POST /like
+        - POST /dislike
+        - POST /retweet
+        - POST /undo
+        - POST /quote
+        - POST /reply
+*/
+router.post('/like', actions.like);
+router.post('/dislike', actions.unlike);
+
+router.post('/retweet', actions.retweet);
+router.post('/undo', actions.undo);
+
+router.post('/quote', actions.quote);
+router.post('/reply', actions.reply);
+
+
+
 
 module.exports = router;
