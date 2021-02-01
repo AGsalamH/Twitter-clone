ca69498636d28e919f9fc87cc58a02cf70ac2255
Ahmed Gamal <agsalamh@gmail.com>
2021-02-01 08:18:34 +0200

    Refactor login & signup

--- users/auth/authentication.js
+++ users/auth/authentication.js
@@ -1,18 +1,15 @@
-const bcrypt = require('bcryptjs');
 const User = require('../userModel');
 const jwt=require('jsonwebtoken');
 
-
-const signup = async (req, res, next) => {
+// POST /signup
+const signup = async (req, res, next) =>{
     // Required
-    const email = req.body.email;
-    const password = req.body.password;
-    const username = req.body.username;
-    const fullname = req.body.fullname;
+    const {email, password, username, fullname} = req.body;
+
     // Optional
     const bio = req.body.bio || null;
     const city = req.body.city || null;
-    let profilePictue;
+    let profilePictue = null;
 
     //Handling image upload
     if(req.file){
@@ -20,18 +17,6 @@ const signup = async (req, res, next) => {
     }
 
     try {
-        // Check If user already exists
-        const userExists = await User.findOne({email: email});
-        if (userExists) {
-            return res.status(400).json({
-                error: 'This Email already Exists, Try another one'
-            });
-        }
-
-        // Here User doesn't exist and ready to be created ...
-
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 12);
         // Save the new User
         const user = new User({
             username,
@@ -40,7 +25,7 @@ const signup = async (req, res, next) => {
             bio,
             city,
             profilePictue,
-            password: hashedPassword
+            password
         });
         const savedUser = await user.save();
         res.status(201).json({
@@ -48,38 +33,17 @@ const signup = async (req, res, next) => {
             userId: savedUser._id
         });        
     } catch (error) {
-        if (!error.statusCode) {
-            error.statusCode = 500;
-        }
         next(error);
     }    
 }
 
 
-const login = async(req,res,next)=>{
-
-    const email = req.body.email;
-    const password = req.body.password;
-
+// POST /login
+const login = async (req,res,next) =>{
     try {
-        // First Check if user exists
-        const user = await User.findOne({email: email});
-        
-        if(!user){
-            return res.status(404).json({
-                error: 'This email Doesn\'t Exist'
-            });
-        }
-        // Here User exist
-        // Let's Check password
-        const isCorrect = await bcrypt.compare(password, user.password);
-    
-        if (!isCorrect) {
-            return res.status(400).json({
-                error: 'Password isn\'t correct'
-            });
-        }
-        
+        const user = await User.emailExists(req.body.email);
+        await user.comparePassword(req.body.password);
+
         // Here Everything is Okay :)
         // Let's create a Token 
         const token = jwt.sign({
@@ -93,9 +57,6 @@ const login = async(req,res,next)=>{
         return res.status(200).json({msg: 'Logged in'});
         
     } catch (error) {
-        if (!error.statusCode) {
-            error.statusCode = 500;
-        }
         next(error);
     }   
 }
