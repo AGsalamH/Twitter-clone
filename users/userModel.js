63def3769d92843f283d36bb815bde6572fbcc6b
Ahmed Gamal <agsalamh@gmail.com>
2021-02-01 08:16:51 +0200

    upgrade user model Add some pre hooks, methods and statics

--- users/userModel.js
+++ users/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
 
 const userSchema = new Schema({
     fullname: {
@@ -47,5 +49,51 @@ const userSchema = new Schema({
 },
     { timestamps: true }
 );
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('email')){
+        return next();
+    }
+    // Check if the email already exists 
+    const user = await this.collection.findOne({email: this.email});
+    if(user){
+        const error = new mongoose.Error('Email Already Exists!');
+        error.statusCode = 400;
+        throw error;
+    }
+    next();
+});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')){
+        return next();
+    }
+    // Hash password Before savind it
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// To be used in login & signup functionality
+userSchema.statics.emailExists = async function(email){
+    const user = await this.findOne({email: email});
+    if(!user){
+        const error = new mongoose.Error('Email doesn\'t exist!');
+        error.statusCode = 404;
+        throw error;
+    }
+    return user;
+}
+
+// used in login
+userSchema.methods.comparePassword = async function(password){
+    const isCorrect = await bcrypt.compare(password, this.password);
+    if(!isCorrect){
+        const error = new mongoose.Error('Password isn\'t correct');
+        error.statusCode = 400;
+        throw error;
+    }
+    return isCorrect;
+}
+
 
 module.exports = mongoose.model('User', userSchema);
