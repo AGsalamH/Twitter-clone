2ba13c571099e727293fe58a253bc0a0455b6e23
Ahmed Gamal <agsalamh@gmail.com>
2021-02-05 11:43:33 +0200

    Add: error handling utility But NOT used yet

---
+++ utils/errorHandling.js
@@ -0,0 +1,28 @@
+const mongooseError = require('mongoose').Error;
+
+// Check if it's a mongoose related error 
+// To help me catch mongoose errors only
+const isMongooseError = err => err instanceof mongooseError;
+
+// Check if it's a Jsonwebtoken error
+const jwtError = err => {
+    const filter = (
+        err.name === 'JsonWebTokenError' ||
+        err.name === 'NotBeforeError'    ||
+        err.name === 'TokenExpiredError'
+    );
+    return filter;
+}
+
+// Just to throw errors in a ternary operator.
+const _throw = err =>{
+    throw err;
+}
+
+
+module.exports = {
+    isMongooseError,
+    jwtError,
+
+    mongooseError
+}
